23af30be66eae392a6d72e5e871cab74b7d0363d
kasia <ksapko97@gmail.com>
2023-06-09 02:50:27 +0200

    jj

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,19 +7,19 @@ import { AiFillFacebook } from "react-icons/ai";
 const HeaderSocials = () => {
   return (
     <div className="header-socials">
-      <a href="https://github.com/ksapko" target="_blank">
+      <a href="https://github.com/ksapko" target="_blank" rel="noreferrer">
         <AiOutlineGithub />
       </a>
       <a
         href="https://www.linkedin.com/in/katarzyna-sapko-825aa8203/"
-        target="_blank"
+        target="_blank" rel="noreferrer"
       >
         <AiFillLinkedin />
       </a>
-      <a href="https://www.instagram.com/kasia_sapko/" target="_blank">
+      <a href="https://www.instagram.com/kasia_sapko/" target="_blank" rel="noreferrer">
         <AiOutlineInstagram />
       </a>
-      <a href="https://www.facebook.com/kasia.sapko/" target="_blank">
+      <a href="https://www.facebook.com/kasia.sapko/" target="_blank" rel="noreferrer">
         <AiFillFacebook />
       </a>
     </div>
