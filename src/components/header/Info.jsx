9d6fafd27b6113e61827cc32ecd3661856f3e21a
kasia <ksapko97@gmail.com>
2023-06-09 20:18:30 +0200

    modify

--- src/components/header/Info.jsx
+++ src/components/header/Info.jsx
@@ -4,9 +4,12 @@ import CV from "../../../src/asserts/CV.pdf";
 const Info = () => {
   return (
     <div className="info">
-      <a href={CV} download className="btn">Download CV
+      <a href={CV} download className="btn">
+        Download CV
       </a>
-      <a href="#contact" className="btn btn-primary">Let's talk</a>
+      <a href="#contact" className="btn btn-primary">
+        Let's talk
+      </a>
     </div>
   );
 };
