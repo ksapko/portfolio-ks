2aad9f7b38df316dc9182e228f7f145ba8c2e05b
kasia <ksapko97@gmail.com>
2023-06-09 20:09:26 +0200

    modify

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,14 +5,15 @@ import car from "./../../../src/asserts/car.jpg";
 const About = () => {
   return (
     <section id="about">
-      <h2>About me</h2>
-      <h3 className="text-about text-light">
-        I love to spend my free time assembling Lego models. It's not only way
-        to relax, but it's also great for developing my creativity and precision
-        skills.
-      </h3>
-      <img className="lego-car" src={car} alt="" />
-      <div className="container about-container"></div>
+      <div className="container about-container">
+        <h2 className="about-title">About me</h2>
+        <h3 className="text-about text-light">
+          I love to spend my free time assembling Lego models. It's not only a
+          way to relax, but it's also great for developing my creativity and
+          precision skills.
+        </h3>
+        <img className="lego-car" src={car} alt="" />
+      </div>
     </section>
   );
 };
